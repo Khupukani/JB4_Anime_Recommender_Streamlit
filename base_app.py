387efaa69a7cea91685841382b2850f2370a3496
Khupukani Maluleke <khupukanimaluleke@gmail.com>
2024-08-08 23:08:39 +0200

    adding data files

--- base_app.py
+++ base_app.py
@@ -7,10 +7,10 @@ from surprise import Dataset, Reader
 
 # Load data
 def load_data():
-    anime = pd.read_csv('data/anime.csv')
-    ratings = pd.read_csv('data/ratings.csv')
-    train = pd.read_csv('data/train.csv')
-    test = pd.read_csv('data/test.csv')
+    anime = pd.read_csv('anime.csv')
+    ratings = pd.read_csv('ratings.csv')
+    train = pd.read_csv('train.csv')
+    test = pd.read_csv('test.csv')
     return anime, ratings, train, test
 
 anime, ratings, train, test = load_data()
